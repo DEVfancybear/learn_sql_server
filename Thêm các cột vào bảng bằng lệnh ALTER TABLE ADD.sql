233377adc0cfb0d85f92e10d79dbf86f27613d09
DuongLT <duonglt.work@gmail.com>
2022-04-23 22:44:50 +0700

    ALTER TABLE - ADD Column

---
+++ Thêm các cột vào bảng bằng lệnh ALTER TABLE ADD.sql
@@ -0,0 +1,10 @@
+﻿-- Thêm các cột vào bảng bằng lệnh ALTER TABLE ADD
+
+alter table [dbo].[userDetail]
+add name2 varchar(50)
+
+alter table userDetail2 add sex varchar(50)
+
+alter table userDetail3
+add sex varchar(20),
+dob date

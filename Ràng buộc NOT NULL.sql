2d4f2cb36876e7a40dc5e769fdc9ff55db582cf6
DuongLT <duonglt.work@gmail.com>
2022-04-23 23:23:13 +0700

    SQL NOT NULL Constraint

---
+++ Ràng buộc NOT NULL.sql
@@ -0,0 +1,19 @@
+﻿--Ràng buộc NOT NULL (field không được để trống, bắt buộc phải có dữ liệu)
+create table Persons (
+	id int not null,
+	lastName varchar(255) not null,
+	firstName varchar(255) not null,
+	Age int
+)
+
+--cập nhật field not null
+
+alter table userDetail
+alter column id int not null
+
+alter table userDetail2
+alter column Email nvarchar(50) not null
+
+-- xoá bảng
+
+drop table Persons

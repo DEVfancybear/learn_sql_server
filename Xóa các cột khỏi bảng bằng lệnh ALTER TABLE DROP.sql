2e1c4656572d1e4a8b1d51e365f58c3e98af70be
DuongLT <duonglt.work@gmail.com>
2022-04-23 22:51:22 +0700

    ALTER TABLE - DROP COLUMN

---
+++ Xóa các cột khỏi bảng bằng lệnh ALTER TABLE DROP.sql
@@ -0,0 +1,14 @@
+﻿--Xóa các cột khỏi bảng bằng lệnh ALTER TABLE DROP
+
+alter table userDetail
+drop column name
+
+
+-- ALTER TABLE - ADD COLUMN
+alter table userDetail add name varchar(50), gender varchar(10)
+
+-- ALTER TABLE - REMOVE MULTI COLUMN	
+
+alter table userDetail2 drop column name, sex, avg
+
+alter table userDetail3 drop column name, avg

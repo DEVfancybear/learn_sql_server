0dd4f4410f5724045856be6e157748bfa2057f5e
DuongLT <duonglt.work@gmail.com>
2022-04-23 22:24:36 +0700

    train sql

---
+++ Tạo và xóa bảng bằng câu truy vấn SQL.sql
@@ -0,0 +1,23 @@
+﻿
+
+--tạo bảng
+create table userDetail(
+	id int,
+	name varchar(50),
+	address varchar(100),
+	email varchar(50),
+	avg float
+);
+
+create table userName(
+	iid int,
+	name varchar(50),
+	address varchar(50),
+	email varchar(50),
+	avg float
+)
+
+--xoá bảng đã tạo
+
+drop table [dbo].[userDetail]
+drop table userName

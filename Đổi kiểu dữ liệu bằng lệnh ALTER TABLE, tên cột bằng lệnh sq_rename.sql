665a3e5899c99a62d7d8f425b43c57e0d71c9015
DuongLT <duonglt.work@gmail.com>
2022-04-23 23:04:52 +0700

    ALTER TABLE - ALTER/MODIFY COLUMN

---
+++ Đổi kiểu dữ liệu bằng lệnh ALTER TABLE, tên cột bằng lệnh sq_rename.sql
@@ -0,0 +1,14 @@
+﻿
+--Đổi kiểu dữ liệu bằng lệnh ALTER TABLE, tên cột bằng lệnh sq_rename
+
+--thay đổi kiểu dữ liệu
+alter table userDetail
+alter column address varchar(50)
+
+--thay đổi tên cột
+--example: table userDetail2 change name columnn email -> Email
+sp_rename 'userDetail2.email', 'Email', 'column'
+
+alter table userDetail2
+alter column Email nvarchar(50)
+

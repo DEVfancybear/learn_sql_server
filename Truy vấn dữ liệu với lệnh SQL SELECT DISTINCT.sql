be035922263541980eaf680091a1e31a285145ff
DuongLT <duonglt.work@gmail.com>
2022-04-24 23:43:20 +0700

    SQL SELECT DISTINCT Statement

---
+++ Truy vấn dữ liệu với lệnh SQL SELECT DISTINCT.sql
@@ -0,0 +1,9 @@
+﻿-- SELECT DISTINCT: chọn ra bộ dữ liệu không trùng nhau
+select * from Person5
+
+insert into Person5(firstName, lastName, age) values ('duong', 'nguyen', 24)
+insert into Person5(firstName, lastName, age) values ('thang', 'pham', 26)
+insert into Person5(firstName, lastName, age) values ('thang', 'linh', 26)
+select distinct firstName from Person5
+
+select distinct age from Person5

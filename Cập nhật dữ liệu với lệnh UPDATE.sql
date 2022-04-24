6a24aece4286fe65550e61d60b965899237eca0f
DuongLT <duonglt.work@gmail.com>
2022-04-24 23:58:17 +0700

    update SQL UPDATE Statement

--- Cập nhật dữ liệu với lệnh UPDATE.sql
+++ Cập nhật dữ liệu với lệnh UPDATE.sql
@@ -2,5 +2,7 @@ update Person5 set firstName = 'duong update' where id = 4
 
 update Person5 set lastName = 'bui hoang' where id = 7
 
+update Person5 set firstName = 'update duong' where firstName = 'duong update'
+
 update Person5 set firstName = 'thangnt', lastName = 'thang nguyen', age = 29 where id = 9
 select * from Person5

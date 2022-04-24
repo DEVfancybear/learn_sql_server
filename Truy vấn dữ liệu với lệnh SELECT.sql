057fa18ff7c71e4c35bf9dd8543c2a14473eab1f
DuongLT <duonglt.work@gmail.com>
2022-04-24 23:38:26 +0700

    SQL SELECT Statement

---
+++ Truy vấn dữ liệu với lệnh SELECT.sql
@@ -0,0 +1,13 @@
+select * from Person5
+
+select firstName, lastName, age from Person5
+
+select lastName from Person5
+
+insert into Person5(firstName, lastName, age) values ('duong', 'le', 35)
+
+insert into Person5(firstName, lastName, age) values ('thang', 'nguyen', 21)
+
+insert into Person5(firstName, lastName, age) values ('lan', 'nguyen', 22)
+
+insert into Person5(firstName, lastName, age) values ('ngoc', 'bui', 23)

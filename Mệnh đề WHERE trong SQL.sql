e883145b19176b9be552740188aa4d49785a4c16
DuongLT <duonglt.work@gmail.com>
2022-04-24 23:49:54 +0700

    update sql WHERE Clause

--- Mệnh đề WHERE trong SQL.sql
+++ Mệnh đề WHERE trong SQL.sql
@@ -5,3 +5,8 @@ select * from Person5 where age = 26
 select id, firstName from Person5 where id = 9
 
 select firstName, lastName from Person5 where firstName = 'duong'
+
+select * from Person5 where age >= 26
+
+
+select * from Person5 where age <= 26

312d29acc452a9ba9f4d93195c7942b705643d4a
DuongLT <duonglt.work@gmail.com>
2022-04-26 22:21:00 +0700

    SQL AND, OR and NOT Operators

---
+++ Toán tử AND OR NOT.sql
@@ -0,0 +1,21 @@
+﻿
+-- and: cả vế trái và phải điều kiện đều phải đúng
+-- or: 1 trong 2 điều kiện đúng
+-- not: không thoả mãn điều kiện
+select * from Person5
+
+select * from Person5 where firstName = 'ngoc' and age > 21
+
+select * from Person5 where firstName = 'duong' or firstName = 'thang'
+
+select * from Person5 where firstName ='sinh' or lastName = 'linh'
+
+select COUNT(*) from Person5 where firstName like '%ng%' and age > 21
+
+select * from Person5 where age > 21 and age <= 30
+
+select * from Person5 where (firstName = 'lan' or firstName = 'ngoc') and age > 22
+
+select * from Person5 where firstName not like '%ng%'
+
+select * from Person5 where lastName not like '%h%'

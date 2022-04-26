3b9f7c4a817077ab43771906d69eb91b9f9f5c75
DuongLT <duonglt.work@gmail.com>
2022-04-26 22:34:14 +0700

    SQL SELECT TOP Clause

---
+++ SELECT TOP vs SELECT TOP PERCENT.sql
@@ -0,0 +1,10 @@
+select top 2 firstName from Person5
+
+select top 1 lastName from Person5 where lastName ='linh'
+
+select * from Person5
+
+select top 30 PERCENT lastName from Person5
+
+
+select top 10 PERCENT firstName from Person5 where firstName ='duong'

7be0983fed1986f88709d63b599f07e1fd6dbe26
DuongLT <duonglt.work@gmail.com>
2022-04-26 22:39:17 +0700

    SQL NULL Values

---
+++ Toán tử IS NULL vs IS NOT NULL.sql
@@ -0,0 +1,6 @@
+﻿select * from Person5
+
+-- hiển thị column có lastName có dữ liệu không null
+select * from Person5 where lastName is not null
+-- hiển thị column có firstName có dữ liệu null
+select * from Person5 where firstName is null

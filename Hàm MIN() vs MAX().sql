2ee92a0dfb631076325a63134e0334c5e152b820
DuongLT <duonglt.work@gmail.com>
2022-04-26 22:57:25 +0700

    SQL MIN() and MAX() Functions

---
+++ Hàm MIN() vs MAX().sql
@@ -0,0 +1,8 @@
+﻿-- tìm ra giá trị nhỏ nhất trong column age
+select min(age) from Person5
+-- tìm ra giá trị lớn nhất trong column age
+select max(age) from Person5
+
+select max(age) as MAX_AGE, min(age) as MIN_AGE from Person5
+
+select max(firstName) as Max_first_name, min(lastName) as min_last_name from Person5

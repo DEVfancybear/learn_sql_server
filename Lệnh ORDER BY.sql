f8e7ff2b35f0c83ae1b16aa1a715f969623744a6
DuongLT <duonglt.work@gmail.com>
2022-04-26 22:27:34 +0700

    SQL ORDER BY Keyword

---
+++ Lệnh ORDER BY.sql
@@ -0,0 +1,15 @@
+﻿-- order by: sắp xếp các bản ghi theo 1 thứ tự, ví dụ: tăng dần theo tên, giảm dần theo tên
+select * from Person5 order by firstName desc
+
+select lastName from Person5 order by lastName asc
+
+select firstName from Person5 order by firstName asc
+
+select lastName from Person5 order by lastName desc
+
+--order by multi column
+
+select * from Person5 order by lastName desc, firstName asc
+
+
+select * from Person5 order by firstName desc, lastName asc

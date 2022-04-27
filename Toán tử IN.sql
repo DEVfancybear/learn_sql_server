e20810f3d827f3897e9ba340fbec58a974f5e0fb
DuongLT <duonglt.work@gmail.com>
2022-04-27 22:43:11 +0700

    SQL IN Operator

---
+++ Toán tử IN.sql
@@ -0,0 +1,13 @@
+﻿-- tìm ra all record có age bằng 35, 26
+select * from Person5 where age in (35,26)
+
+-- tìm ra all record không có age bằng 35, 26
+select * from Person5 where age not in (35,26)
+
+select * from Person5
+
+-- tìm ra all record có firstName là thangnt, duong
+select * from Person5 where firstName in ('thangnt', 'duong')
+
+-- tìm ra all record jkhông có firstName là thangnt, duong
+select * from Person5 where firstName not in ('thangnt', 'duong')

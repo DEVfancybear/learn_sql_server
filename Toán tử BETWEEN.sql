4697051e0ffcfdefa28026b5db414158204837f8
DuongLT <duonglt.work@gmail.com>
2022-04-27 22:51:27 +0700

    SQL BETWEEN Operator

---
+++ Toán tử BETWEEN.sql
@@ -0,0 +1,14 @@
+﻿-- BETWEEN: dùng để lựa chọn các giá trị trong 1 phạm vi hoặc đoạn cho trước
+
+
+-- lấy ra tất cả các record có age nằm từ đoạn 25 đến 30
+select * from Person5 where age between 25 and 30
+
+
+-- lấy ra tất cả các record có age không nằm từ đoạn 25 đến 30
+select * from Person5 where age not between 25 and 30
+-- lấy ra tất cả các record có firstName nằm từ duong đến ngoc 
+select * from Person5 where firstName between 'duong' and 'ngoc'
+
+-- lấy ra tất cả các record có firstName không nằm từ duong đến ngoc 
+select * from Person5 where firstName between 'duong' and 'ngoc'

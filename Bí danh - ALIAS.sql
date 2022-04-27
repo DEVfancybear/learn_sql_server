cfd29e021b59947b12db828c15ccb05c198fde89
DuongLT <duonglt.work@gmail.com>
2022-04-27 22:58:42 +0700

    SQL Aliases

---
+++ Bí danh - ALIAS.sql
@@ -0,0 +1,8 @@
+select p5.age as tuoi, p5.firstName as ten_dau, p5.lastName as ho  from Person5 as p5
+select p5.lastName + ' ' + p5.firstName as ho_ten from Person5 as p5
+select p5.lastName + ',' + p5.firstName as ho_ten from Person5 as p5
+
+
+select p5.firstName as [First Name] from Person5 as p5
+
+select p5.lastName as [Last Name] from Person5 as p5

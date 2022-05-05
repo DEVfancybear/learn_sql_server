23132b96049a0ed0dbab2c2abb270df5989cdb4d
DuongLT <duonglt.work@gmail.com>
2022-05-05 23:20:08 +0700

    RIGHT JOIN

---
+++ RIGHT JOIN.sql
@@ -0,0 +1,7 @@
+﻿--right join: trả về tất cả record bên tay phải và những record tương ứng quan hệ primary key - foreign key bên tay trái
+
+select ctm.*, o3.* from Customers as ctm
+right join Orders3 as o3
+on o3.CustomerID = ctm.CustomerID
+
+select * from Orders3

565c14aa8984e450b8b54c0028564f847b5921a3
DuongLT <duonglt.work@gmail.com>
2022-05-06 00:05:14 +0700

    Self Join

---
+++ Self Join.sql
@@ -0,0 +1,5 @@
+﻿--self join: 
+-- self join: chọn ra bộ dữ liệu thoả mãn 1 điều kiện nào đó trong 1 bảng
+select a.Country, b.Country, a.CustomerName from Customers a, Customers b
+where a.CustomerID> b.CustomerID
+and a.Country = b.Country

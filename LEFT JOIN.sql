166d2fccf6286a0642481eab3218f713824b7618
DuongLT <duonglt.work@gmail.com>
2022-05-05 23:11:36 +0700

    LEFT JOIN

---
+++ LEFT JOIN.sql
@@ -0,0 +1,4 @@
+﻿-- left join: trả về tất cả recoird bên tay trả và các record có điểm chung giữa khoá chính - khoá ngoại ở bảng bên phải
+select cts.* from Customers as cts
+left join Orders3 as o3 on o3.CustomerID = cts.CustomerID
+where cts.CustomerID = 2
